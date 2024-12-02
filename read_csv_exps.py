83963e339af0f82eb2e6980138fc588e7141002a
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2024-12-02 09:11:45 +0800

    some mplfinance plotting.

--- read_csv_exps.py
+++ read_csv_exps.py
@@ -1,17 +1,20 @@
 #%%
+import warnings
+warnings.filterwarnings("ignore")
+def action_with_warnings():
+    warnings.warn("should not appear")
+with warnings.catch_warnings(record=True):
+    action_with_warnings()
 import configparser
-import pandas as pd
+import mplfinance as mpf
+import pandas     as pd
 
 
 #%%
 config = configparser.ConfigParser()
 config.read("config.ini")
-print(config.sections())
 csv_path = config.get('XAUUSD', 'csv_path')
-print(f"path : {csv_path}")
 
-
-#%%
 df = pd.read_csv(f"{csv_path}", header=None, names=[
     'Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'DuplicatedVolume', 'Spread'
 ])
@@ -19,14 +22,107 @@ df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
 df.set_index('Datetime', inplace=True)
 df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
 df.dropna(inplace=True)
+
 df
 
 
 #%%
-df.iloc[-100:]['Close'].plot()
+plot_df = df.iloc[-1000:]
+mpf.plot(plot_df, type='candle', style='binance')
 
 
 #%%
 
 
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
 
